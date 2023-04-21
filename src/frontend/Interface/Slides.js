4472b2af2abdb473bec02713b35eec2e35a1168d
sumaiya-cmd <88318661+sumaiya-cmd@users.noreply.github.com>
2023-04-21 22:49:48 +0530

    corsel work

--- src/frontend/Interface/Slides.js
+++ src/frontend/Interface/Slides.js
@@ -12,12 +12,15 @@ export default function Slide() {
                     <div class="carousel-item active">
                         <div class="row">
                             <div class="col-lg-6 p-5">
-                                <img src="https://i.pinimg.com/originals/73/54/ed/7354ed843c4c4f82c330c9e93b1d71e7.jpg" class="w-100 rounded" style={{ minHeight: "300px", objectFit: "cover" }} alt="pop-1" />
+                                <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_social_share_1024x768_scale,f_auto,q_auto:best/rockcms/2023-01/it-ends-with-us-blake-lively-2x1-jp-230127-298917.jpg" class="w-100 rounded" style={{ minHeight: "300px", objectFit: "cover" }} alt="pop-1" />
                             </div>
                             <div class="col-lg-6 p-5">
                                 <div class="d-none d-md-block text-center">
-                                    <h5 class='p-5 text-light fs-1'>Book 1</h5>
-                                    <p class='fs-5 fst-italic text-light'>Some representative placeholder content for the first slide.</p>
+                                    <h5 class='p-5 text-light fs-1' style={{fontFamily :"Playfair Display"}}>IT ENDS WITH US <br />
+                                        <span  style={{fontFamily:"Playfair Display" , fontSize:"20px" ,opacity:"0.5"}}>by Colleen Hoover</span>
+                                    </h5>
+                                    
+                                    <p class='fs-5 fst-italic text-light' style={{fontFamily:"Oregano"}}>It Ends with Us is a book that follows a girl named Lily who has just moved and is ready to start her life after college. Lily then meets a guy named Ryle and she falls for him. As she is developing feelings for Ryle, Atlas, her first love, reappears and challenges the relationship between Lily and Ryle.</p>
                                 </div>
                             </div>
                         </div>
@@ -25,23 +28,27 @@ export default function Slide() {
                     <div class="carousel-item ">
                         <div class="row">
                             <div class="col-lg-6 p-5">
-                                <img src="/images/Web 1920 – 1.png" class="w-100 rounded" style={{ minHeight: "300px", objectFit: "cover" }} alt="pop-1" />
+                                <img src="https://readingwhale.com/wp-content/uploads/2017/07/all-the-light-we-cannot-see.jpg" class="w-100 rounded" style={{ minHeight: "300px", objectFit: "cover" }} alt="pop-1" />
                             </div>
                             <div class="col-lg-6 p-5">
-                                <h5 class='p-5 text-light fs-1'>Book 2</h5>
-                                <p class='fs-5 fst-italic text-light'>Some representative placeholder content for the first slide.</p>
+                                <h5 class='p-5 text-light fs-1' style={{fontFamily :"Playfair Display"}}>The Light We Cannot See <br />
+                                <span  style={{fontFamily:"Playfair Display" , fontSize:"20px" ,opacity:"0.5"}}>by Anthony Doer</span>
+                                </h5>
+                                <p class='fs-5 fst-italic text-light'style={{fontFamily:"Oregano"}} >When Marie Laure goes blind, aged six, her father builds her a model of their Paris neighbourhood, so she can memorize it with her fingers and then navigate the real streets. But when the Germans occupy Paris, father and daughter flee to Saint-Malo on the Brittany coast, where Marie-Laures agoraphobic great uncle lives in a tall, narrow house by the sea wall. In another world in Germany, an orphan boy, Werner, is enchanted by a crude radio. He becomes a master at building and fixing radios, a talent ultimately makes him a highly specialized tracker of the Resistance..</p>
                             </div>
                         </div>
                     </div>
                     <div class="carousel-item">
                         <div class="row">
                             <div class="col-lg-6 p-5">
-                                <img src="https://2halftruth.files.wordpress.com/2017/03/book-trees-knowledge-life.jpg?w=1272" class="w-100 rounded" style={{ minHeight: "300px", objectFit: "cover" }} alt="pop-1" />
+                                <img src="https://ekn27r2hcs5.exactdn.com/wp-content/uploads/2021/08/Verity-Jasmine.png?strip=all&lossy=1&ssl=1" class="w-100 rounded" style={{ minHeight: "300px", objectFit: "cover" }} alt="pop-1" />
                             </div>
                             <div class="col-lg-6 p-5">
                                 <div class="d-none d-md-block text-center">
-                                    <h5 class='p-5 text-light fs-1'>Book 3</h5>
-                                    <p class='fs-5 fst-italic text-light'>Some representative placeholder content for the first slide.</p>
+                                    <h5 class='p-5 text-light fs-1'style={{fontFamily :"Playfair Display"}} >verity <br />
+                                    <span  style={{fontFamily:"Playfair Display" , fontSize:"20px" ,opacity:"0.5"}}>by Colleen Hoover</span>
+                                    </h5>
+                                    <p class='fs-5 fst-italic text-light'>Verity focuses on the life of Lowen Ashleigh, a young writer living in New York City struggling for money and not really living the life she envisioned. One day she bumps into a man who will change her life forever in more ways than one.</p>
                                 </div>
                             </div>
                         </div>
